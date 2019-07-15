f065bc7f9bc60b962f4e5544ac7808b6e309552a
Liangdi <wu@liangdi.me>
2019-07-15 17:18:38 +0800

    handle exit app

--- lib/pages/workshop_page.dart
+++ lib/pages/workshop_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class WorkshopPage extends StatefulWidget {
@@ -9,46 +11,71 @@ class WorkshopPage extends StatefulWidget {
 class _WorkshopPageState extends State<WorkshopPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Theme.of(context).canvasColor,
-        title: Padding(
-          padding: EdgeInsets.only(left: 8.0, right: 8.0),
-          child: TextField(
-            decoration: InputDecoration(
-                hintText: '请输入关键字', suffixIcon: Icon(Icons.search)),
+    Future<bool> _onWillPop() {
+      return showDialog(
+            context: context,
+            builder: (context) => new AlertDialog(
+              title: new Text('提示'),
+              content: new Text('是否退出程序'),
+              actions: <Widget>[
+                new FlatButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: new Text('否'),
+                ),
+                new FlatButton(
+                  onPressed: () => exit(0),
+                  child: new Text('是'),
+                ),
+              ],
+            ),
+          ) ??
+          false;
+    }
+
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          backgroundColor: Theme.of(context).canvasColor,
+          title: Padding(
+            padding: EdgeInsets.only(left: 8.0, right: 8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: '请输入关键字', suffixIcon: Icon(Icons.search)),
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/profile');
+                  },
+                  icon: Icon(Icons.person, color: Colors.grey)),
+            )
+          ],
+        ),
+        body: Center(
+          child: Text('Workshop List'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          onPressed: () {
+            print('scanning');
+          },
+          child: Icon(
+            Icons.crop_free,
+            color: Colors.grey,
           ),
         ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 16),
-            child: IconButton(
-              onPressed: (){
-                Navigator.pushReplacementNamed(context, '/profile');
-              },
-              icon: Icon(Icons.person,color: Colors.grey)),
-          )
-        ],
-      ),
-      body: Center(
-        child: Text('Workshop List'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        onPressed: () {
-          print('scanning');
-        },
-        child: Icon(
-          Icons.crop_free,
-          color: Colors.grey,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Container(
-          height: 38.0,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          child: Container(
+            height: 38.0,
+          ),
         ),
       ),
     );
