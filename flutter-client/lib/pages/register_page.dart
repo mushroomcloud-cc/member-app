50bc559befab34d5e3ecfbea80e0513f8b68ece6
sumu <felicia_sumu@gmail.com>
2019-07-17 14:51:31 +0800

    first version for textform of register_page

--- flutter-client/lib/pages/register_page.dart
+++ flutter-client/lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 
 class RegisterPage extends StatefulWidget {
   RegisterPage({Key key}) : super(key: key);
@@ -7,6 +8,11 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  
+  TextEditingController _unameController =new TextEditingController();
+  TextEditingController _pwdController = new TextEditingController();
+  GlobalKey _formKey = new GlobalKey<FormState>();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +45,72 @@ class _RegisterPageState extends State<RegisterPage> {
                     style: TextStyle(letterSpacing: 8.0),
                   ),
                 ),
-                 FlatButton(
-                  child: Text("确认"),
-                  onPressed: ()=>{},
-                ),
               ],
-            )
+            ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.only(left: 30,right: 30),
+              child: Form(
+                key: _formKey,
+                autovalidate: true,
+                child: Column(
+                  children: <Widget>[
+                  
+                    TextFormField(
+                      autofocus: true,
+                      controller: _unameController,
+                      decoration: InputDecoration(
+                        labelText: "用户名",
+                        hintText: "请输入用户名",
+                        icon: Icon(Icons.person)
+                      ),
+                      validator: (v){
+                        return v
+                            .trim()
+                            .length >0?null :"用户名不能为空";
+                      },
+                    ),
+                    TextFormField(
+                      controller: _pwdController,
+                      decoration:InputDecoration(
+                        labelText: "密码",
+                        hintText: "请输入密码",
+                        icon: Icon(Icons.lock)
+                      ),
+                      obscureText: true,
+                      validator: (v){
+                        return v
+                            .trim()
+                            .length >5? null :"密码不能少于6位";
+                      }
+                    ),
+                  ],
+                ),
+              ),
+            ),
+             Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:Row(
+                 children: <Widget>[
+                   Expanded(
+                     child: RaisedButton(
+                      padding: const EdgeInsets.all(8.0),
+                         child: Text("提交"),
+                         color:Theme
+                              .of(context)
+                              .primaryColor,
+                         textColor: Colors.white,
+                         onPressed:(){
+                           if((_formKey.currentState as FormState).validate())
+                           {
+
+                           }
+                         },
+                     ),
+                   )
+                 ],
+               ) 
+             ),
           ],
         ),
       ),
