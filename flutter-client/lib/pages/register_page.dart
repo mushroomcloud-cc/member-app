d23bb06d32de2361cc33b505008424b0e0887f85
sumu <felicia.sumu@gmail.com>
2019-07-18 21:11:21 +0800

    add unformatted content and jump to login_page

--- flutter-client/lib/pages/register_page.dart
+++ flutter-client/lib/pages/register_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 
 class RegisterPage extends StatefulWidget {
   RegisterPage({Key key}) : super(key: key);
@@ -8,11 +7,13 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  
-  TextEditingController _unameController =new TextEditingController();
+  TextEditingController _unameController = new TextEditingController();
   TextEditingController _pwdController = new TextEditingController();
+  TextEditingController _pnController = new TextEditingController();
   GlobalKey _formKey = new GlobalKey<FormState>();
-  
+
+  bool _checkboxSelected = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,70 +48,78 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
               ],
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 60,
+            ),
             Padding(
-              padding: const EdgeInsets.only(left: 30,right: 30),
+              //padding: const EdgeInsets.only(left: 30,right: 30),
+              padding: const EdgeInsets.only(left: 60, right: 60),
               child: Form(
                 key: _formKey,
                 autovalidate: true,
                 child: Column(
                   children: <Widget>[
-                  
                     TextFormField(
                       autofocus: true,
                       controller: _unameController,
                       decoration: InputDecoration(
-                        labelText: "用户名",
-                        hintText: "请输入用户名",
-                        icon: Icon(Icons.person)
+                        labelText: "姓名",
+                        hintText: "请输入姓名",
+                        //icon: Icon(Icons.person)
                       ),
-                      validator: (v){
-                        return v
-                            .trim()
-                            .length >0?null :"用户名不能为空";
+                      validator: (v) {
+                        return v.trim().length > 0 ? null : "姓名不能为空";
                       },
                     ),
                     TextFormField(
-                      controller: _pwdController,
-                      decoration:InputDecoration(
-                        labelText: "密码",
-                        hintText: "请输入密码",
-                        icon: Icon(Icons.lock)
+                        controller: _pwdController,
+                        decoration: InputDecoration(
+                          labelText: "密码",
+                          hintText: "请输入密码",
+                          //icon: Icon(Icons.lock)
+                        ),
+                        obscureText: true,
+                        validator: (v) {
+                          return v.trim().length > 5 ? null : "密码不能少于6位";
+                        }),
+                    TextFormField(
+                      controller: _pnController,
+                      decoration: InputDecoration(
+                        labelText: "手机号",
+                        hintText: "请输入手机号",
+                        //icon: Icon(Icons.phone)
                       ),
-                      obscureText: true,
-                      validator: (v){
-                        return v
-                            .trim()
-                            .length >5? null :"密码不能少于6位";
-                      }
+                      validator: (v) {
+                        return v.trim().length > 10 ? null : "请输入有效的手机号";
+                      },
                     ),
                   ],
                 ),
               ),
             ),
-             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child:Row(
-                 children: <Widget>[
-                   Expanded(
-                     child: RaisedButton(
-                      padding: const EdgeInsets.all(8.0),
-                         child: Text("提交"),
-                         color:Theme
-                              .of(context)
-                              .primaryColor,
-                         textColor: Colors.white,
-                         onPressed:(){
-                           if((_formKey.currentState as FormState).validate())
-                           {
-
-                           }
-                         },
-                     ),
-                   )
-                 ],
-               ) 
-             ),
+            SizedBox(
+              height: 100,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(right: 100, left: 100),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text("提交"),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        onPressed: () {
+                          if ((_formKey.currentState as FormState).validate()) {
+                            Navigator.pushNamed(context, '/login');
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                )),
           ],
         ),
       ),
